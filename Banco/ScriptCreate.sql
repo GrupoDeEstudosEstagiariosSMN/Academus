c78b3bd761079060e17e96f30f9956bfdf753d32
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-05 16:24:37 -0300

    Configs do banco de dados sqlserver

--- Banco/ScriptCreate.sql
+++ Banco/ScriptCreate.sql
@@ -7,3 +7,12 @@ CREATE TABLE usuario (
 	CONSTRAINT pk_usuario PRIMARY KEY (id),
 	CONSTRAINT uq_usuario_email UNIQUE (email)
 );
+
+CREATE TABLE usuario (
+    id int identity(1,1),
+    nome varchar(64) NOT NULL,
+    email varchar(128) NOT NULL,
+    senha varchar(64) NOT NULL,
+    CONSTRAINT pk_usuario PRIMARY KEY CLUSTERED (id),
+	CONSTRAINT uq_usuario_email UNIQUE (email)
+);
