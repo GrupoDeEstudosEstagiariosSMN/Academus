efb6e68a64ac60722d31e57443923dfb6e99e14a
Andressa Abrantes <andressaabrantes@gmail.com>
2023-05-08 16:37:12 -0300

    View do Financeiro e controller financeiro

---
+++ Core/Models/Fornecedor.cs
@@ -0,0 +1,12 @@
+
+namespace Core.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Representante { get; set; }
+        public string Produto { get; set; }
+    }
+}
---
+++ Web/Controllers/FinanceiroController.cs
@@ -0,0 +1,10 @@
+namespace Web.Controllers
+{
+    public class FinanceiroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
